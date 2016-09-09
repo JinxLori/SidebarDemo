428a4dedbe181348a43fd3853d38e0688bbe485b
jasonZhang <www.fully@qq.com>
2016-09-09 09:44:48 +0800

    init

---
+++ library/src/main/java/com/jason/library/widget/Sidebar.java
@@ -0,0 +1,23 @@
+package com.jason.library.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author zjh
+ * @date 2016/9/9
+ */
+public class Sidebar extends View{
+    public Sidebar(Context context) {
+        super(context);
+    }
+
+    public Sidebar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Sidebar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
